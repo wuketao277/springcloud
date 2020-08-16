fbb4db0f43dcd412d7cf24ab712b71687065abb1
wuketao <wuketao@houbank.com>
2020-08-16 16:46:21 +0800

    eureka项目初始状态

---
+++ eureka/src/main/java/com/hello/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.hello.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author wuketao
+ * @date 2020/08/16
+ * @Description
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
