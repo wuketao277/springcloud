3490454d35aa319a0f8f6f7ee63f8974b148a39e
wuketao277 <howard.wu@helloapplicant.com>
2024-06-13 09:54:11 +0800

    完成gateway项目

--- OrderService/src/main/java/com/hello/orderservice/controller/OrderMsgController.java
+++ OrderService/src/main/java/com/hello/orderservice/controller/OrderMsgController.java
@@ -5,14 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 /**
  * @author wuketao
- * @date 2024/6/7
+ * @date 2024/6/13
  * @Description
  */
-@RestController("OrderController")
-public class OrderController {
+@RestController("order")
+public class OrderMsgController {
     @Autowired
     private OrderService orderService;
 
